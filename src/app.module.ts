e9cbc188f95a4e5e616dda582f2795c11353920f
MiguelAngelsp <miguel.correopers@gmail.com>
2022-11-20 19:58:29 +0100

    DockerizarPostgreSQL_integracionTypeORM_ConexionBD_20-11-2022

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,22 @@ import { UsuariosModule } from './usuarios/usuarios.module';
 import { PeliculasModule } from './peliculas/peliculas.module';
 import { ValoracionesModule } from './valoraciones/valoraciones.module';
 import { GenerosModule } from './generos/generos.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsuariosModule, PeliculasModule, ValoracionesModule, GenerosModule],
+  imports: [ ConfigModule.forRoot(), TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: !!process.env.DB_SYNC
+    }),
+    UsuariosModule, PeliculasModule, ValoracionesModule, GenerosModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
